bbcb5a76feb3642113f8208ebb34231441ed1f46
WSRer <1749094641@qq.com>
2025-01-26 11:15:19 +0000

    update

--- learning-lm-rs/src/model.rs
+++ learning-lm-rs/src/model.rs
@@ -309,24 +309,11 @@ fn mlp(
     rms_w: &Tensor<f32>,
     eps: f32,
 ) {
-    use std::f32::consts::E;
-    OP::rms_norm(hidden_states,residual, rms_w, eps);
+    OP::rms_norm(hidden_states, residual, rms_w, eps);
     OP::matmul_transb(gate, 0., hidden_states, w_gate, 1.);
     OP::matmul_transb(up, 0., hidden_states, w_up, 1.);
-    let up_data = up.data().to_vec();
-    let mut act_data = gate.data().to_vec();
-    for idx in 0..act_data.len() {
-        act_data[idx] = act_data[idx] * (1.0 / (1.0 + E.powf(-act_data[idx])))
-        * up_data[idx];
-    }
-    let mut act = Tensor::new(act_data, gate.shape());
-    let mut output = Tensor::new(act.data().to_vec(), act.shape());
-    OP::matmul_transb(&mut output, 0., &mut act, w_down, 1.);
-    let residual_data = unsafe {residual.data_mut()};
-    let output_data = output.data();
-    for idx in 0..residual_data.len() {
-        residual_data[idx] += output_data[idx];
-    }
+    OP::swiglu(up, gate);
+    OP::matmul_transb(residual, 1., up, w_down, 1.);
 }
 
 #[test]
